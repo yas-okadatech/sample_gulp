706507e7a31162326a6eec205757e2b3a4ce5613
Yas Okada <yas.okadatech@gmail.com>
2015-10-25 20:51:51 +0900

    Fix #1 modified gulpfile.js for es6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,25 @@
-var gulp = require('gulp');
+import gulp from 'gulp';
 
-var size = require('gulp-size');
+import size from'gulp-size';
 
-var dirNode = './node_modules';
+let dirNode = './node_modules';
 
-var dirSrc = './src';
-var dirDest = './dest';
+let dirSrc = './src';
+let dirDest = './dest';
 
-gulp.task('assets', ['assets:font-awesome', 'assets:bootstrap'], function () {
+gulp.task('assets', ['assets:font-awesome', 'assets:bootstrap'], () => {
   gulp.src([dirSrc + '/assets/**/*'], {base: dirSrc + '/assets/'})
     .pipe(size({title: 'assets'}))
     .pipe(gulp.dest(dirDest));
 });
 
-gulp.task('assets:font-awesome', function () {
+gulp.task('assets:font-awesome', () => {
   gulp.src([dirNode + '/font-awesome/fonts/**/*'], {base: dirNode + '/font-awesome/'})
     .pipe(size({title: 'font-awesome'}))
     .pipe(gulp.dest(dirDest));
 });
 
-gulp.task('assets:bootstrap', function () {
+gulp.task('assets:bootstrap', () => {
   gulp.src([dirNode + '/bootstrap-sass/assets/fonts/**/*'], {base: dirNode + '/bootstrap-sass/assets/'})
     .pipe(size({title: 'bootstrap'}))
     .pipe(gulp.dest(dirDest));
